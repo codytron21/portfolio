ad33568d95dc47c1bcc0291827065a084c49986f
rohan <rohannak2057@gmail.com>
2022-12-25 23:42:18 +0530

    adds social media icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.jpeg";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
@@ -10,10 +11,10 @@ const Header = () => {
         <h1>Rohan Kumar</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="profilePic" />
         </div>
-
         <a href="#contact" className="scroll_down">
           Scroll Down
         </a>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 const HeaderSocials = () => {
   return (
-    <div className="header_social">
-      <a href="" target="_blank"></a>
-      <a href="" target="_blank"></a>
+    <div className="header_socials">
+      <a href="" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="" target="_blank">
+        <FaGithub />
+      </a>
       <a href="" target="_blank"></a>
     </div>
   );
