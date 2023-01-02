3027317eddba7f418bfbcecd5e5503a5635f8cbc
rohan <rohannak2057@gmail.com>
2023-01-02 22:32:41 +0530

    adds about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,44 @@
 import React from "react";
-
+import "./about.css";
+import coding from "../../assets/coding.gif";
+import { FaAward } from "react-icons/fa";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-  return <section id="about"></section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={coding} alt="coding gif" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icons" />
+              <h5>Experience</h5>
+              <small>1+ Years of Full-stack</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icons" />
+              <h5>Projects</h5>
+              <small>5+</small>
+            </article>
+          </div>
+          <p>
+            Hi, I'm a Full-stack Developer Possesing Complete skill to develop
+            an application.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
