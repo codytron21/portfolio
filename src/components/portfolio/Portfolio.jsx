3e02510728104b882d06be9cdfd336d64fa08068
rohan <rohannak2057@gmail.com>
2023-01-04 22:09:05 +0530

    adds portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import "./portfolio.css";
+import PortfolioImg from "../../assets/sample.jpeg";
+import { data } from "./portfolioData";
 const Portfolio = () => {
-  return <section id="portfolio"></section>;
+  return (
+    <section className="portfolio">
+      <h5>My Recent Work</h5>
+      <h2> Portfolio</h2>
+      <div className="container portfolio__container">
+        {data.map(({ id, image, github, demo }) => {
+          console.log(image);
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={PortfolioImg} alt="portfolio" />
+              </div>
+              <h3>This is a portfolio item title</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
---
+++ src/components/portfolio/portfolioData.js
@@ -0,0 +1,52 @@
+
+const data = [
+    {
+        id: 1,
+        image: 'PortfolioImg',
+        title: 'A Full-stack Application',
+        github: 'https://www.github.com',
+        demo: 'https://www.google.com'
+    },
+    {
+        id: 2,
+        image: 'PortfolioImg',
+        title: 'A Full-stack Application',
+        github: 'https://www.github.com',
+        demo: 'https://www.google.com'
+    },
+    {
+        id: 3,
+        image: 'PortfolioImg',
+        title: 'A Full-stack Application',
+        github: 'https://www.github.com',
+        demo: 'https://www.google.com'
+    },
+    {
+        id: 4,
+        image: 'PortfolioImg',
+        title: 'A Full-stack Application',
+        github: 'https://www.github.com',
+        demo: 'https://www.google.com'
+    },
+    {
+        id: 5,
+        image: 'PortfolioImg',
+        title: 'A Full-stack Application',
+        github: 'https://www.github.com',
+        demo: 'https://www.google.com'
+    },
+    {
+        id: 6,
+        image: 'PortfolioImg',
+        title: 'A Full-stack Application',
+        github: 'https://www.github.com',
+        demo: 'https://www.google.com'
+    }
+
+]
+
+module.exports =
+
+{
+    data,
+}
